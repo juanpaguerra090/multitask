8969b9a1f4627a72cfa77e7a15b099303ea42e5d
MiguelCbrm <miguel_angel_cabral@hotmail.com>
2016-04-22 08:03:45 -0500

    VUE.js

---
+++ JSHello.js
@@ -0,0 +1,49 @@
+var load = function(){
+	new Vue({
+		el: '#app',
+		data: {
+	    	message: 'Hello'
+ 		}
+	})
+
+	new Vue({
+  		el: '#app1',
+  		data: {
+   			message: 'Hello'
+  		}
+	})
+
+	new Vue({
+ 		el: '#app2',
+  		data: {
+    	message: 'Hello Vue'
+ 			},
+  		methods: {
+    	reverseMessage: function () {
+     	this.message = this.message.split('').reverse().join('')
+    		}
+  		}
+	})
+
+	new Vue({
+	  	el: '#app3',
+	  	data: {
+	    	newTodo: '',
+	    	todos: [
+	    	  	{ text: 'Hello Tacos' }
+	    	]
+	  	},
+	  	methods: {
+	    	addTodo: function () {
+	      	var text = this.newTodo.trim()
+	      	if (text) {
+	        	this.todos.push({ text: text })
+	        	this.newTodo = ''
+	      	}
+	    },
+	    	removeTodo: function (index) {
+	     		this.todos.splice(index, 1)
+	    	}
+	  	}
+	})
+};
